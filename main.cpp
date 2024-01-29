0272fee5f9e913d37a8529e92275631230de730e
Your Name <you@example.com>
2024-01-29 21:20:58 +0200

    file extintion checking

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <stack>
 #include <string>
 #include <system_error>
@@ -10,11 +11,22 @@ bool isvalid(std::string s) ;
 std :: string file_name ; 
 
 int main (int argc, char *argv[]) {
+
 	if (argc  < 2 )
+	{
 		std::cerr << "Invalid !! there is no file neame !\n";
+		return -1 ; 
+	}
 	else 
 		file_name = argv[1];
-	std :: cout << file_name;
+	
+	//file name checking 
+
+	if (std::string extintion (file_name.end ()- 3 , file_name.end()); extintion != ".bf")
+		std:: cout << extintion;
+	else 
+		std:: cout << extintion << "e" ; 
+
 	return 0;
 }
 bool isvalid(std::string s) {
