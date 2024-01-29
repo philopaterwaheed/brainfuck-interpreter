f2d7ec911274b1d73fdd22113de6662578bbce0e
Your Name <you@example.com>
2024-01-29 05:36:05 +0200

    added getting file name

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <stack>
+#include <string>
+#include <system_error>
+//funcs 
 bool isvalid(std::string s) ;
+
+
+// vars 
+std :: string file_name ; 
+
 int main (int argc, char *argv[]) {
-	
-	while (true)
-	{
-		std :: string x ; 
-		std :: cin >> x ; 
-		std :: cout << isvalid(x);
-	}
+	if (argc  < 2 )
+		std::cerr << "Invalid !! there is no file neame !\n";
+	else 
+		file_name = argv[1];
+	std :: cout << file_name;
 	return 0;
 }
 bool isvalid(std::string s) {
